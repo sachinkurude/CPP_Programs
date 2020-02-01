f84319b83cd0129539ab871f9ff4c8e60eb3d83b
sachin kurude <scnkurude@gmail.com>
2020-02-01 16:58:53 +0530

    Multiple Inheritance....done

---
+++ basicSalaryCalNetSalInOppsMultipleInheritance.cpp
@@ -0,0 +1,49 @@
+/*
+ * basicSalaryCalNetSalInOppsMultipleInheritance.cpp
+ *
+ *  Created on: 31-Jan-2020
+ *      Author: sachin kurude
+ */
+#include<iostream>
+using namespace std;
+class EmpAccept
+{
+public:
+	int bSal;
+	void accept()
+	{
+		cout<<"Accept Basic Salary:"<<endl;
+		cin>>bSal;
+	}
+
+};
+class EmpCal
+{
+	public:
+	int bonus,netSal;
+
+};
+class EmpResult:public EmpCal,public EmpAccept
+{
+public:
+	void cal()
+	{
+		bonus=bSal*0.15;
+		netSal=bSal+bonus;
+	}
+	void result()
+	{
+		cout<<"Employee Basic Salary = "<<bSal<<endl;
+		cout<<"Employee Bonus = "<<bonus<<endl;
+		cout<<"Employee Net Salary = "<<netSal<<endl;
+	}
+};
+int main()
+{
+	EmpResult E;
+	E.accept();
+	E.cal();
+	E.result();
+	return 0;
+
+}
