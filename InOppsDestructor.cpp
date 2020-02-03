5c5babe086e344fb2ffe6f849adc51b444f620c0
sachin kurude <scnkurude@gmail.com>
2020-02-03 14:17:27 +0530

    Destructor....

---
+++ InOppsDestructor.cpp
@@ -0,0 +1,26 @@
+/*
+ * InOppsDestructor.cpp
+ *
+ *  Created on: 01-Feb-2020
+ *      Author: sachin kurude
+ */
+#include <iostream>
+using namespace std;
+class Employee
+ {
+   public:
+        Employee()
+        {
+            cout<<"Constructor Invoked"<<endl;
+        }
+        ~Employee()
+        {
+            cout<<"Destructor Invoked"<<endl;
+        }
+};
+int main(void)
+{
+    Employee e1; //creating an object of Employee
+    Employee e2; //creating an object of Employee
+    return 0;
+}
