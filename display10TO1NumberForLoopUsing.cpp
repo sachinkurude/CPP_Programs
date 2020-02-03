00b3b87594fa03261c8d893ba9c622eacdbef42a
sachin kurude <scnkurude@gmail.com>
2020-02-04 04:54:59 +0530

    display using loop..

---
+++ display10TO1NumberForLoopUsing.cpp
@@ -0,0 +1,19 @@
+/*
+ * display10TO1NumberForLoopUsing.cpp
+ *
+ *  Created on: 03-Feb-2020
+ *      Author: sachin kurude
+ */
+//print 1 to 1 number using for loop
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i;
+	for(i=10;i>=1;i--)
+	{
+		cout<<i<<endl;
+	}
+	return 0;
+
+}
---
+++ display1TO10NumberForLoopUsing.cpp
@@ -0,0 +1,19 @@
+/*
+ * display1TO10NumberForLoopUsing.cpp
+ *
+ *  Created on: 03-Feb-2020
+ *      Author: sachin kurude
+ */
+//print 1 to 10 number using for loop
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i;
+	for(i=1;i<=10;i++)
+	{
+		cout<<i<<endl;
+	}
+	return 0;
+
+}
