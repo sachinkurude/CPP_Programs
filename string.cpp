94626abd7c06d6a190c23d2e9a4769020016ac01
sachin kurude <scnkurude@gmail.com>
2020-02-03 14:12:32 +0530

    String.

---
+++ string.cpp
@@ -0,0 +1,16 @@
+/*
+ * string.cpp
+ *
+ *  Created on: 01-Feb-2020
+ *      Author: sachin kurude
+ */
+//string......
+#include <iostream>
+using namespace std;
+int main( ) {
+    string s1 = "Hello";
+        char ch[] = { 'C', '+', '+'};
+        string s2 = string(ch);
+        cout<<s1<<endl;
+        cout<<s2<<endl;
+}
