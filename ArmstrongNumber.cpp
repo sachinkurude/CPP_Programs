a55d6863fd6e25d6a2a64f7a1c5ccb3313a29b1f
sachin kurude <scnkurude@gmail.com>
2020-02-04 04:57:40 +0530

    Armstrong..number

---
+++ ArmstrongNumber.cpp
@@ -0,0 +1,27 @@
+/*
+ * ArmstrongNumber.cpp
+ *
+ *  Created on: 03-Feb-2020
+ *      Author: sachin kurude
+ */
+//Armstrong Number
+#include <iostream>
+using namespace std;
+int main()
+{
+int n,r,sum=0,temp;
+cout<<"Enter the Number=  ";
+cin>>n;
+temp=n;
+while(n>0)
+{
+r=n%10;
+sum=sum+(r*r*r);
+n=n/10;
+}
+if(temp==sum)
+cout<<"Armstrong Number."<<endl;
+else
+cout<<"Not Armstrong Number."<<endl;
+return 0;
+}
