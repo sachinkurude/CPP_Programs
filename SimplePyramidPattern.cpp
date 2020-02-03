5349c74de77d1c331a04b95f6a91c2d5dc294b4c
sachin kurude <scnkurude@gmail.com>
2020-02-04 04:46:36 +0530

    Simple pyramid pattern

---
+++ SimplePyramidPattern.cpp
@@ -0,0 +1,37 @@
+/*
+ * SimplePyramidPattern.cpp
+ *
+ *  Created on: 03-Feb-2020
+ *      Author: sachin kurude
+ */
+// C++ code to demonstrate star pattern
+#include <iostream>
+using namespace std;
+
+// Function to demonstrate printing pattern
+void pypart(int n)
+{
+    // outer loop to handle number of rows
+    //  n in this case
+    for (int i=0; i<n; i++)
+    {
+        //  inner loop to handle number of columns
+        //  values changing acc. to outer loop
+        for(int j=0; j<=i; j++ )
+        {
+            // printing stars
+            cout << "* ";
+        }
+
+        // ending line after each row
+        cout << endl;
+    }
+}
+
+// Driver Function
+int main()
+{
+    int n = 5;
+    pypart(n);
+    return 0;
+}
